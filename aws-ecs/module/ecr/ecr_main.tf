ab9d4358f38b6721ff18f860c524241b76fdd18a
anandan06 <118799667+anandan06@users.noreply.github.com>
2024-07-23 07:05:53 +0530

    Delete aws-ecs directory

--- aws-ecs/module/ecr/ecr_main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_ecr_repository" "my_ecr_repo" {
-  name                 = "my-ecr-repo"
-  image_tag_mutability = "MUTABLE"
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
--- aws-ecs/module/ecs/cluster.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecs_cluster" "my_ecs_cluster" {
-  name = "my-ecs-cluster"
-}
-
-output "cluster_id" {
-  value = aws_ecs_cluster.my_ecs_cluster.id
-}
-
